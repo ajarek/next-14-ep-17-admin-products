26ea54a76c68e8fd672671981d044e26820802e4
Ajarek <ajarek@poczta.onet.pl>
2023-12-03 00:16:25 +0100

    added mongoDB

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,12 +1,79 @@
-import { Metadata } from 'next';
- 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from "@/components/ui/label"
+import { revalidatePath } from 'next/cache'
+
+import { Metadata } from 'next'
+import { z } from 'zod'
+import dbConnect from '@/lib/db-connect'
+import ProductModel from '@/lib/product-model'
 export const metadata: Metadata = {
   title: 'Create Products',
-};
-
+}
+const userSchema = z.object({
+  image: z.string(),
+  name: z.string(),
+  price: z.string(),
+  rating: z.string(),
+})
 const Page = () => {
+  async function create(formData: FormData) {
+    'use server'
+    const productData = userSchema.parse({
+      image: formData.get('image'),
+      name: formData.get('name'),
+      price: formData.get('price'),
+      rating: formData.get('rating'),
+    })
+    if (!productData) {
+      
+      return { message: 'Form data is not valid' }
+    }
+   
+    
+      await dbConnect()
+      const product = new ProductModel(productData)
+      console.log(product);
+      
+      await product.save()
+      revalidatePath('/')
+      return { message: `Created product ${productData.name}` }
+    
+   
+    
+  }
   return (
-    <div>Create Products</div>
+    <form action={create} className='p-24 flex flex-col justify-evenly gap-4'>
+      <Label htmlFor="image">Image Address</Label>
+      <Input
+        type='text'
+        name='image'
+        required
+        placeholder='https://freepngimg.com/thumb/tshirt/20-t-shirt-png-image-thumb.png'
+      />
+       <Label htmlFor="name">Product Name</Label>
+      <Input
+        type='text'
+        name='name'
+        required
+        placeholder='Shirt'
+      />
+       <Label htmlFor="price">Product Price $</Label>
+      <Input
+        type='text'
+        name='price'
+        required
+        placeholder='520.25'
+      />
+       <Label htmlFor="rating">Rating</Label>
+      <Input
+        type='text'
+        name='rating'
+        required
+        placeholder='⭐⭐⭐⭐⭐'
+      />
+      <Button className='bg-green-400 text-lg' type='submit'>Submit</Button>
+    </form>
   )
 }
 
---
+++ lib/product-model.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+export type Product = {
+  _id: string
+  name: string
+  image: string
+  price: string
+  rating: string
+}
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    image: { type: String, required: true },
+    price: { type: String, required: true },
+    rating: { type: String, required: true,  },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const ProductModel =
+  mongoose.models.Product || mongoose.model('Product', productSchema)
+export default ProductModel
